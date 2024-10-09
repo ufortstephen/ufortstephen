96554be994ecf53100fab2bf497e298d570b33bd
Ufort Stephen <ufortstephen@gmail.com>
2024-10-09 19:02:10 +0100

    new projects

--- src/components/Experiences.vue
+++ src/components/Experiences.vue
@@ -1,104 +1,103 @@
 <template>
-  <div
-    class="max-w-100 mx-auto px-6 lg:px-20 mt-5 lg:mt-20 pb-20"
-    id="experience"
-  >
+  <div class="max-w-100 mx-auto px-6 lg:px-20 mt-5 lg:mt-20 pb-20" id="experience">
     <div class="grid">
-      <h1
-        class="leading-tight text-xl md:text-2xl mt-0 mb-2 text-white font-bold font-mono"
-      >
+      <h1 class="leading-tight text-xl md:text-2xl mt-0 mb-2 text-white font-bold font-mono">
         <span class="text-yellow font-mono md:pr-0">02.</span> Experiences
       </h1>
     </div>
-    <div
-      class="md:flex gap-20 md:space-x-10 md:pl-20 mt-10 space-y-20 md:space-y-0"
-    >
+    <div class="md:flex gap-20 md:space-x-10 md:pl-20 mt-10 space-y-20 md:space-y-0">
       <div class="md:flex items-start gap-20">
         <ul
           class="nav lg:basis-2/4 nav-tabs font-mono flex md:flex-col flex-nowrap md:flex-wrap list-none border-b-0 pl-0 mr-4 overflow-scroll md:overflow-hidden"
-          id="tabs-tabVertical"
-          role="tablist"
-        >
+          id="tabs-tabVertical" role="tablist">
           <li class="nav-item flex-grow text-center" role="presentation">
-            <a
-              href="#tabs-blockmayVertical"
+            <a href="#tabs-AutoMagic"
               class="w-max nav-link block font-medium text-md md:text-xl leading-tight border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent focus:border-transparent active py-5"
-              id="tabs-blockmay-Vertical"
-              data-bs-toggle="pill"
-              data-bs-target="#tabs-blockmayVertical"
-              role="tab"
-              aria-controls="tabs-blockmayVertical"
-              aria-selected="false"
-              >Blockmay
+              id="tabs-automagic-Vertical" data-bs-toggle="pill" data-bs-target="#tabs-AutoMagic" role="tab"
+              aria-controls="tabs-AutoMagic" aria-selected="false">AutoMagic
             </a>
           </li>
           <li class="nav-item flex-grow text-center" role="presentation">
-            <a
-              href="#tabs-homeVertical"
-              class="w-max nav-link block font-medium text-md md:text-xl leading-tight border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent focus:border-transparent py-5"
-              id="tabs-home-tabVertical"
-              data-bs-toggle="pill"
-              data-bs-target="#tabs-homeVertical"
-              role="tab"
-              aria-controls="tabs-homeVertical"
-              aria-selected="false"
-              >OneWatt Solar</a
-            >
+            <a href="#tabs-profileVertical"
+              class="w-max nav-link block font-medium text-xs md:text-xl leading-tight border-x-0 border-t-0 border-b-2 border-transparent px-6 my-2 hover:border-transparent focus:border-transparent py-5 hover:opacity-80 hover:text-green"
+              id="tabs-profile-tabVertical" data-bs-toggle="pill" data-bs-target="#tabs-profileVertical" role="tab"
+              aria-controls="tabs-profileVertical" aria-selected="false">SmilePay Africa</a>
           </li>
           <li class="nav-item flex-grow text-center" role="presentation">
-            <a
-              href="#tabs-profileVertical"
-              class="w-max nav-link block font-medium text-xs md:text-xl leading-tight border-x-0 border-t-0 border-b-2 border-transparent px-6 my-2 hover:border-transparent focus:border-transparent py-5 hover:opacity-80 hover:text-green"
-              id="tabs-profile-tabVertical"
-              data-bs-toggle="pill"
-              data-bs-target="#tabs-profileVertical"
-              role="tab"
-              aria-controls="tabs-profileVertical"
-              aria-selected="false"
-              >SmilePay Africa</a
-            >
+            <a href="#tabs-blockmayVertical"
+              class="w-max nav-link block font-medium text-md md:text-xl leading-tight border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent focus:border-transparent py-5"
+              id="tabs-blockmay-Vertical" data-bs-toggle="pill" data-bs-target="#tabs-blockmayVertical" role="tab"
+              aria-controls="tabs-blockmayVertical" aria-selected="false">Blockmay
+            </a>
           </li>
+          <li class="nav-item flex-grow text-center" role="presentation">
+            <a href="#tabs-homeVertical"
+              class="w-max nav-link block font-medium text-md md:text-xl leading-tight border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent focus:border-transparent py-5"
+              id="tabs-home-tabVertical" data-bs-toggle="pill" data-bs-target="#tabs-homeVertical" role="tab"
+              aria-controls="tabs-homeVertical" aria-selected="false">OneWatt Solar</a>
+          </li>
+
           <li class="w-max nav-item flex-grow text-center" role="presentation">
-            <a
-              href="#tabs-messagesVertical"
+            <a href="#tabs-messagesVertical"
               class="nav-link block font-medium text-xs md:text-xl leading-tight border-x-0 border-t-0 border-b-2 border-transparent px-6 my-2 hover:border-transparent focus:border-transparent py-5 hover:opacity-80 hover:text-green"
-              id="tabs-messages-tabVertical"
-              data-bs-toggle="pill"
-              data-bs-target="#tabs-messagesVertical"
-              role="tab"
-              aria-controls="tabs-messagesVertical"
-              aria-selected="false"
-              >Start Innovation Hub</a
-            >
+              id="tabs-messages-tabVertical" data-bs-toggle="pill" data-bs-target="#tabs-messagesVertical" role="tab"
+              aria-controls="tabs-messagesVertical" aria-selected="false">Start Innovation Hub</a>
           </li>
         </ul>
         <div class="tab-content" id="tabs-blockmayContentVertical">
-          <div
-            class="tab-pane fade show active"
-            id="tabs-blockmayVertical"
-            role="tabpanel"
-            aria-labelledby="tabs-blockmay-tabVertical"
-          >
-            <h1
-              class="leading-tight text-xl md:text-2xl mt-0 mb-2 text-white font-bold font-mono"
-              style="font-family: 'DM Sans', sans-serif"
-            >
-              Blockchain Engineer
-              <span class="text-red md:pr-0"> @ Blockmay</span>
+          <div class="tab-pane fade show active" id="tabs-AutoMagic" role="tabpanel"
+            aria-labelledby="tabs-AutoMagic-tabVertical">
+            <h1 class="leading-tight text-xl md:text-2xl  lg:w-9/12 lg:ml-auto mt-0 mb-2 text-white font-bold font-mono"
+              style="font-family: 'DM Sans', sans-serif">
+              Software Engineer
+              <span class="text-red md:pr-0"> @ AutoMagic</span>
             </h1>
 
-            <h4 class="text-md md:text-md text-gray-300 my-5">June 2021</h4>
+            <h4 class="text-md md:text-md text-gray-300 my-5 lg:w-9/12 lg:ml-auto">June 2023</h4>
+            <div class="text-justify mx-5 md:mx-10">
+              <ul class="list-disc text-gray-300 lg:w-9/12 lg:ml-auto">
+                <li class="text-sm md:text-lg my-5">
+                  As a Frontend Engineer for Automagic, I specialize in rapidly creating user interfaces for initial
+                  product
+                  versions. I transform client ideas into functional, visually appealing applications, focusing on
+                  delivering
+                  high-quality, user-friendly experiences within deadlines.
+                </li>
+                <li class="text-sm md:text-lg my-5">
+                  Developing and launching multiple MVPs, enabling startups to validate their ideas and attract initial
+                  funding.
+                </li>
+                <li class="text-sm md:text-lg my-5">
+                  Creating and implementing creative design solutions that addresses unique client needs that
+                  enables them stand out in competitive markets.
+                </li>
+                <li class="text-sm md:text-lg my-5">
+                  Consistently delivering high-quality, user-friendly interfaces that meets and exceeds clients
+                  expectations, resulting in positive feedback and repeat business.
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="tab-pane fade show" id="tabs-blockmayVertical" role="tabpanel"
+            aria-labelledby="tabs-blockmay-tabVertical">
+            <h1 class="leading-tight text-xl md:text-2xl mt-0 lg:w-9/12 mb-2 text-white font-bold font-mono"
+              style="font-family: 'DM Sans', sans-serif">
+              Blockchain Engineer
+              <span class="text-red md:pr-0 "> @ Blockmay</span>
+            </h1>
+
+            <h4 class="text-md md:text-md text-gray-300 my-5 lg:w-9/12">June 2021</h4>
             <div class="text-justify mx-5 md:mx-10">
               <ul class="list-disc text-gray-300 lg:w-9/12">
-                <li class="text-sm md:text-md my-5">
+                <li class="text-sm md:text-lg my-5">
                   Led the development of a Decentralized Social Media
                   Application and other Blockchain based applications.
                 </li>
-                <li class="text-sm md:text-md my-5">
+                <li class="text-sm md:text-lg my-5">
                   Built a Decentralized Social Media Platform (FIDLE) that
                   allows it's users earn rewards for creating content.
                 </li>
-                <li class="text-sm md:text-md my-5">
+                <li class="text-sm md:text-lg my-5">
                   Utilized modern JavaScript frameworks, such as React, Vue,
                   Web3.js and Ether.js, to create seamless and intuitive user
                   experiences and interact with the Blockchain.
@@ -106,63 +105,51 @@
               </ul>
             </div>
           </div>
-          <div
-            class="tab-pane fade show"
-            id="tabs-homeVertical"
-            role="tabpanel"
-            aria-labelledby="tabs-home-tabVertical"
-          >
-            <h1
-              class="leading-tight text-xl md:text-2xl mt-0 mb-2 text-white font-bold font-mono"
-              style="font-family: 'DM Sans', sans-serif"
-            >
+          <div class="tab-pane fade show" id="tabs-homeVertical" role="tabpanel"
+            aria-labelledby="tabs-home-tabVertical">
+            <h1 class="leading-tight text-xl md:text-2xl mt-0 mb-2 lg:w-9/12 text-white font-bold font-mono"
+              style="font-family: 'DM Sans', sans-serif">
               FrontEnd Developer
               <span class="text-yellow md:pr-0"> @ OneWatt Solar</span>
             </h1>
 
-            <h4 class="text-md md:text-md text-gray-300 my-5">November 2021</h4>
+            <h4 class="text-md md:text-md text-gray-300 my-5 lg:w-9/12">November 2021</h4>
             <div class="text-justify mx-5 md:mx-10">
               <ul class="list-disc text-gray-300 lg:w-9/12">
-                <li class="text-sm md:text-md my-5">
+                <li class="text-sm md:text-lg my-5">
                   Developed and maintained the company's Internet of Things
                   (IoT) platform, enabling seamless integration of connected
                   devices intothe e-commerce ecosystem.
                 </li>
-                <li class="text-sm md:text-md my-5">
+                <li class="text-sm md:text-lg my-5">
                   Built a Decentralized Powered Digital Electricity Platform.
                 </li>
-                <li class="text-sm md:text-md my-5">
+                <li class="text-sm md:text-lg my-5">
                   Created, maintained, and enforced front-end code and
                   documentation standards.
                 </li>
               </ul>
             </div>
           </div>
-          <div
-            class="tab-pane fade"
-            id="tabs-profileVertical"
-            role="tabpanel"
-            aria-labelledby="tabs-profile-tabVertical"
-          >
-            <h1
-              class="leading-tight text-xl md:text-2xl mt-0 mb-2 text-white font-bold font-mono"
-              style="font-family: 'DM Sans', sans-serif"
-            >
+          <div class="tab-pane fade" id="tabs-profileVertical" role="tabpanel"
+            aria-labelledby="tabs-profile-tabVertical">
+            <h1 class="leading-tight text-xl md:text-2xl lg:w-9/12 mt-0 mb-2 text-white font-bold font-mono"
+              style="font-family: 'DM Sans', sans-serif">
               FrontEnd Developer
               <span class="text-green md:pr-0"> @ SmilePay Africa</span>
             </h1>
 
-            <h4 class="text-sm md:text-md text-gray-300 my-5">2022</h4>
+            <h4 class="text-sm md:text-md text-gray-300 my-5 lg:w-9/12">2022</h4>
             <div class="text-justify mx-5 md:mx-10">
               <ul class="list-disc text-gray-300 lg:w-9/12">
-                <li class="text-sm md:text-md my-5">
+                <li class="text-sm md:text-lg my-5">
                   Building Africa's Foremost Payment Platform
                 </li>
-                <li class="text-sm md:text-md my-5">
+                <li class="text-sm md:text-lg my-5">
                   Implemented intuitive user experience with React, Twilio,
                   jQuery.
                 </li>
-                <li class="text-sm md:text-md my-5">
+                <li class="text-sm md:text-lg my-5">
                   Creating, maintaining, and enforcing front-end code and
                   documentation standards.
                 </li>
@@ -170,36 +157,30 @@
               </ul>
             </div>
           </div>
-          <div
-            class="tab-pane fade"
-            id="tabs-messagesVertical"
-            role="tabpanel"
-            aria-labelledby="tabs-profile-tabVertical"
-          >
-            <h1
-              class="leading-tight text-xl md:text-2xl mt-0 mb-2 text-white font-bold font-mono"
-              style="font-family: 'DM Sans', sans-serif"
-            >
+          <div class="tab-pane fade" id="tabs-messagesVertical" role="tabpanel"
+            aria-labelledby="tabs-profile-tabVertical">
+            <h1 class="leading-tight text-xl md:text-2xl mt-0 lg:w-9/12 mb-2 text-white font-bold font-mono"
+              style="font-family: 'DM Sans', sans-serif">
               Frontend Mentor
               <span class="text-blue md:pr-0"> @ Start Innovation Hub</span>
             </h1>
 
-            <h4 class="text-md md:text-xl text-gray-300 my-5">January 2021</h4>
+            <h4 class="text-md md:text-xl text-gray-300 my-5 lg:w-9/12">January 2021</h4>
             <div class="text-justify mx-5 md:mx-10">
               <ul class="list-disc text-gray-300 lg:w-9/12">
-                <li cclass="text-sm md:text-md my-5">Self learning.</li>
-                <li cclass="text-sm md:text-md my-5">
+                <li cclass="text-sm md:text-lg my-5">Self learning.</li> <br />
+                <li cclass="text-sm md:text-lg my-5">
                   Mentored over 50 stedents on acquiring valuable Web
                   Development Skills.
-                </li>
-                <li cclass="text-sm md:text-md my-5">
+                </li> <br />
+                <li cclass="text-sm md:text-lg my-5">
                   Created more than 20 web application landing pages with HTML5
                   and CSS3.
-                </li>
-                <li cclass="text-sm md:text-md my-5">
+                </li> <br />
+                <li cclass="text-sm md:text-lg my-5">
                   Used CSS/Bootstrap to design multiple responsive web apps.
-                </li>
-                <li cclass="text-sm md:text-md my-5">More learning....</li>
+                </li> <br />
+                <li cclass="text-sm md:text-lg my-5">More learning....</li>
               </ul>
             </div>
           </div>
@@ -217,6 +198,7 @@ export default {};
 .nav-tabs .nav-link {
   position: relative;
 }
+
 .nav-tabs .nav-link.active::after {
   content: "";
   position: absolute;
@@ -227,6 +209,7 @@ export default {};
   background-color: #84e296;
   border-radius: 20px;
 }
+
 .nav-tabs .nav-link.active {
   color: #fff;
   /* border-color: #2563eb; */
